b6e1d1da68e9977bc97bb01332f14ecd87ee1dcb
jsakhinana <jagadesh.sakhinana@gmail.com>
2022-04-13 09:33:34 +0530

    list python

---
+++ collections.py
@@ -0,0 +1,10 @@
+my_list = [2,5,7,11,9,15,22,35,20,99]
+
+print(my_list)
+print(my_list[0:3])
+print(my_list[3:])
+print(my_list[::])
+print(my_list[1:6:2])
+print(my_list[1::2])
+print(my_list[9::-1])
+print(my_list[9:5:-1])
